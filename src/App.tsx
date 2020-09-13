4731585b21b4e12e449cf86d40bb7f570c4314a1
Egor Gorbachev <banderror@gmail.com>
2020-09-13 18:11:07 +0200

    Add a header and description to the app

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,18 @@ import RomanNumeralsConverter from './components/RomanNumeralsConverter';
 function App() {
   return (
     <div className="app">
+      <header className="app__header">
+        <h1>Roman Numerals Converter</h1>
+      </header>
+      <div className="app__description">
+        <p>
+          Type a normal integer number on the left text area and see its representation as a Roman numeral on the right.
+          <br />
+          You can input multiple numbers using multiple lines.
+          <br />
+          Or type roman numerals on the right side and see the opposite conversion working as well.
+        </p>
+      </div>
       <div className="app__converter">
         <RomanNumeralsConverter />
       </div>
